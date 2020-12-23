853f3526a3ef33d52022eb8eb9d2091d1d61a2a3
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-23 14:17:28 -0500

    first commit

---
+++ src/Gallery.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import PICTURES from './data/pictures';
+
+const SECONDS = 1000;
+const minimumDelay = 1 * SECONDS;
+const minimumIncrement = 1;
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [delay, setDelay] = useState(3 * SECONDS);
+  const [increment, setIncrement] = useState(1);
+
+  useEffect(() => {
+    console.log('delay', delay);
+
+    const interval = setInterval(() => {
+      setIndex(storedIndex => (storedIndex + increment) % PICTURES.length);
+    }, delay);
+    return () => {
+      console.log('remove last interval');
+      clearInterval(interval);
+    };
+  }, [delay, increment]);
+
+  console.log('index', index);
+
+  const updateDelay = event => {
+    const delay = Number(event.target.value) * SECONDS;
+
+    setDelay(delay < minimumDelay ? minimumDelay : delay);
+  };
+
+  const updateIncrement = event => {
+    const increment = Number(event.target.value);
+
+    setIncrement(increment < minimumIncrement ? minimumIncrement : increment);
+  };
+
+  return (
+    <div className="Gallery">
+      <img src={PICTURES[index].image} alt="gallery" />
+      <div className="multiform">
+        <div>
+          Gallery transition delay (seconds):
+          <input type="number" onChange={updateDelay} min="1" />
+        </div>
+        <div>
+          Gallery increment:
+          <input type="number" onChange={updateIncrement} min="1" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Tasks.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import uuid from 'uuid/v4';
+
+const TASKS_STORAGE_KEY = 'TASKS_STORAGE_KEY';
+
+const storeTasks = taskMap => {
+  localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(taskMap));
+};
+
+const readStoredTasks = () => {
+  const tasksMap = JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY));
+
+  return tasksMap ? tasksMap : { tasks: [], completedTasks: [] };
+};
+
+export default function Tasks() {
+  const [taskText, setTaskText] = useState('');
+  const storedTasks = readStoredTasks();
+  const [tasks, setTasks] = useState(storedTasks.tasks);
+  const [completedTasks, setCompletedTasks] = useState(
+    storedTasks.completedTasks
+  );
+
+  useEffect(() => {
+    storeTasks({ tasks, completedTasks });
+  });
+
+  const updateTaskText = event => {
+    setTaskText(event.target.value);
+  };
+
+  const addTask = () => {
+    setTasks([...tasks, { taskText, id: uuid() }]);
+    setTaskText('');
+  };
+
+  const completeTask = completedTask => () => {
+    setCompletedTasks([...completedTasks, completedTask]);
+    setTasks(tasks.filter(task => task.id !== completedTask.id));
+  };
+
+  const deleteTask = task => () => {
+    setCompletedTasks(completedTasks.filter(t => t.id !== task.id));
+  };
+
+  console.log('tasks', tasks);
+  console.log('completedTasks', completedTasks);
+
+  return (
+    <div>
+      <h3>Tasks</h3>
+      <div className="form">
+        <input type="text" value={taskText} onChange={updateTaskText} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="task-list">
+        {tasks.map(task => {
+          const { id, taskText } = task;
+          return (
+            <div key={id} onClick={completeTask(task)}>
+              {taskText}
+            </div>
+          );
+        })}
+      </div>
+      <div className="completed-list">
+        {completedTasks.map(task => {
+          const { id, taskText } = task;
+          return (
+            <div key={id}>
+              {taskText}{' '}
+              <span className="delete-task" onClick={deleteTask(task)}>
+                x
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
